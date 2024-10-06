958d3a3b2323146d91773e66a2c9265aac1603e6
madhan-0407 <128352105+madhan-0407@users.noreply.github.com>
2024-10-06 11:26:23 +0530

    Create vehicle.py

---
+++ vehicle.py
@@ -0,0 +1,7 @@
+class vehicle:
+    def tata(Model,colour,brand):
+        print("Model is:",Model)
+        print("brand is:",brand)
+        print("colour is:",colour)
+cars=vehicle 
+cars.tata("tata1761","NavyBlue","tata")
